9b58c119a5a16371a4f9f3259b6bdcb4ba8d0489
kristof22122 <kristf44@gmail.com>
2021-03-09 18:12:40 +0200

    fix: js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    let a
+    let b = ''
+    let c
+    a = String(n)
+    for (let i of a){
+      b = i + b
+    } 
+    c = Number(b)  
+    return c  
 }
